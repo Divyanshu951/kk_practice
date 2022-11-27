92c9e02bb6449afb9d57f0585ee0df642a9db84d
Divyanshu <kdivyanshu951@gmail.com>
2022-11-27 23:38:32 +0530

    repo

--- src/selectionsort.java
+++ src/selectionsort.java
@@ -3,9 +3,32 @@ import java.util.Arrays;
 public class selectionsort {
     public static void main(String[] args) {
         int[] arr = {5, 3, 4, 1, 2};
-        slcsort(arr);
+        cycsort(arr);
         System.out.println(Arrays.toString(arr));
 
+    }
+    public static void cycsort(int[] arr){
+        int i = 0;
+        while(i < arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct]){
+                swap(arr,i,correct);
+            }else{
+                i++;
+            }
+        }
+    }
+    public static void insesort(int[] arr){
+        for(int i = 0; i < arr.length - 1; i++){
+            for(int j = i + 1; j > 0; j--){
+                if(arr[j] < arr[j - 1]){
+                    swap(arr,j,j-1);
+                }else{
+                    break;
+                }
+            }
+
+        }
     }
     public static void slcsort(int[] arr){
         for (int i = 0; i < arr.length; i++) {
